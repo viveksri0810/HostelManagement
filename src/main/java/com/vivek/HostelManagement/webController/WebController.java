748be401d9f81e6c0e3d26658ece310f70b55f70
vivek <viveksri0810@gmail.com>
2024-05-22 05:48:32 -0300

    fixing thymeleaf url

--- src/main/java/com/vivek/HostelManagement/webController/WebController.java
+++ src/main/java/com/vivek/HostelManagement/webController/WebController.java
@@ -7,8 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     @GetMapping("/")
+    public String getLandingPage(){
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
     public String getHomePage(){
         return "home";
+    }
+
+    @GetMapping("/listProperties")
+    public String getListProperties(){
+        return "listProperties";
+    }
+
+    @GetMapping("/help")
+    public String getHelp(){
+        return "help";
     }
 
 
